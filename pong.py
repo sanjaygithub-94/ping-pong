7942aaaf9582bc23961f6eb9aa364fb77069db9d
sanjaygithub-94 <sanjaysajeev7@gmail.com>
2022-08-20 18:01:12 +0000

    push for the learning rate 1e-5

--- pong.py
+++ pong.py
@@ -9,9 +9,9 @@ import time
 
 
 # hyperparameters
-H = 600 # number of hidden layer neurons
+H = 200 # number of hidden layer neurons
 batch_size = 10 # every how many episodes to do a param update?
-learning_rate = 1e-4
+learning_rate = 1e-5
 gamma = 0.99 # discount factor for reward
 decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
 resume = False # resume from previous checkpoint?
